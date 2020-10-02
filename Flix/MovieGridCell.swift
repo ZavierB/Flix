10ac94706d2126ed2a886ca72243824de988811e
Xavier Berolo <XBerolo@icloud.com>
2020-10-02 19:49:46 -0400

    Assignment 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by X on 10/2/20.
+//  Copyright © 2020 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
